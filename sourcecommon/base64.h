9a95b0ae71d5ddb936e289b3280ca885d64fd665
Shaked123456 <shaked@wizards.co.il>
2023-09-04 13:00:06 +0300

    ADD

--- sourcecommon/base64.h
+++
@@ -1,7 +0,0 @@
-#ifndef base64H
-#define base64H
-
-char * base64_encode(const unsigned char *input, int length);
-unsigned char * base64_decode(const char *input, int length, int *outlen);
-
-#endif
